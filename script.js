2f53a5c6d2ee5bc665b49596b7904aa36f759a58
JohnM89 <mjammer2@gmail.com>
2024-07-09 23:28:06 -0400

    got red tiles to populate correctly

--- script.js
+++ script.js
@@ -26,13 +26,29 @@ class Square extends Canvas{
 
 }
 // draw a 10 x 10 grid
+//TODO 
+//get this class working
 class GameTiles extends Canvas {
+
+    
 
     drawGrid(x, y, colour) {
         this.ctx.fillStyle = colour;
         this.ctx.fillRect(x, y, 10, 10);
 
     }
+
+    greenTiles(gameTiles){
+    for(let i = 0; i < gameTiles.length; i++){
+        let min = 0;
+        let max = 10;
+        if(Math.floor(Math.random() * (max - min + 1)) + min === 3){
+            console.log(gameTiles[i].join(",").replace("White", "Red").split(","))
+            gameTiles[i].join(",").replace(/^(white|green)\b/g, "Red").split(",")
+        }
+        return greenTiles
+    }
+}
 
     
     static generateTiles() {
@@ -53,7 +69,7 @@ class GameTiles extends Canvas {
     }
 }
 
-
+// temp setup 
 let newCanvas = new Canvas("#canvasId")
 let square1 = new Square("#canvasId")
 let newGame1 = new GameTiles('#canvasId')
@@ -63,15 +79,15 @@ square1.drawSquare(10, 0, 'green' )
 
 
 
-// temp setup 
 
+// Create tiles 
 const gameTiles = []
 for(let x = 0; x < 10; x++){
     for(let y = 0; y < 10; y++){
         gameTiles.push([y* 10, x * 10])
     }   
 }
-// DIVIDE BY 20 MAYBE AND USE MATH.FLOOR TO ROUND? NOPE LOL
+// Colour tiles
 for(let i = 0; i < gameTiles.length; i++){
 
 
@@ -97,30 +113,40 @@ for(let i = 0; i < gameTiles.length; i++){
 //         gameTiles[i].push("Green") }
 //  }       
 
-for(let i = 0; i < gameTiles.length; i++){
-    for(let value of gameTiles){
-        let param;
-        param += value
-        square1.drawSquare(param)
-    }
-}
+// for(let i = 0; i < gameTiles.length; i++){
+//     for(let value of gameTiles){
+//         let param;
+//         param += value
+//         square1.drawSquare(param)
+//     }
+// }
 
-
-const greenTiles = (gameTiles) => {
+//append Red Tiles
+//TODO
+//randomization needs to be better spaced 
+function redTiles(gameTiles) {
     for(let i = 0; i < gameTiles.length; i++){
         let min = 0;
         let max = 10;
         if(Math.floor(Math.random() * (max - min + 1)) + min === 3){
-            gameTiles[i][2].replace(/Green|White/g, "Red")
+        gameTiles[i][2] = "Red"
         }
+        // console.log(gameTiles)
     }
 }
+
+//Draw map
+gameTiles.forEach(tiles => square1.drawSquare(...tiles))
+//Call redTiles filter
+redTiles(gameTiles);
+//Draw map with redTiles
+gameTiles.forEach(tiles => square1.drawSquare(...tiles))
 
 // greenTiles(gameTiles)
 //problem was i need the spread operator, i was passing the entire array as a single argument it needs to be "spread" across the 3 expected arguments
 // could have also selected each subarray tiles[0] tiles[1]
 //works but its a checkerboard I want
 // need to iterate over the array in a way that checks the tens columns as well as the 1s for equal or odd 
-gameTiles.forEach(tiles => square1.drawSquare(...tiles))
+
 
 
